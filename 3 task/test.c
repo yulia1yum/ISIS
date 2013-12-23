bf7f9cd10934b0a830f3bbdd85233653f70d440a
Yuliya Yumatova <yumatova.yulia@gmail.com>
2013-12-23 20:12:37 +0400

    test

---
+++ 3 task/test.c
@@ -0,0 +1,11 @@
+#define FUSE_USE_VERSION 29
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include "fsfileops.h"
+
+int main(int argc, char *argv[]){
+
+}
